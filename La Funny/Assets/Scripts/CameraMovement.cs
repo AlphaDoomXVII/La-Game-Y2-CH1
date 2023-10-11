75e36164dcd7695b752dcb9a43d297d2b0b5d839
AlphaDoomXVII <514345@vistacollege.nl>
2023-10-11 13:07:51 +0200

    Safe push
    just to be safe

--- La Funny/Assets/Scripts/CameraMovement.cs
+++ La Funny/Assets/Scripts/CameraMovement.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    //veriables
     public Transform player;
 
     [Header("Camera movement")]
     public float mouseSensitivity = 2f;
     float cameraVerticalRotation = 0f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
---
+++ La Funny/Assets/Scripts/StaminaManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaHandeler : MonoBehaviour
+{
+    [Header("")]
+    public Image StaminaBar;
+    public float Stamina = 100f;
+    void Update()
+    {
+        
+    }
+
+    private void StaminaDeplete()
+    {
+
+    }
+
+    private void StaminaRegen()
+    {
+
+    }
+}
