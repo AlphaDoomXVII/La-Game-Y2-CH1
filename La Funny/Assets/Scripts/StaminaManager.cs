38fa30144a1b8bc0b6d1f8356cdc788d88f99161
AlphaDoomXVII <514345@vistacollege.nl>
2023-10-12 11:25:51 +0200

    Stamina Sytem en Scene name change
    en nog wat andere kleine changes

--- La Funny/Assets/Scripts/StaminaManager.cs
+++ La Funny/Assets/Scripts/StaminaManager.cs
@@ -1,25 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StaminaHandeler : MonoBehaviour
+public class StaminaManager : MonoBehaviour
 {
-    [Header("")]
-    public Image StaminaBar;
-    public float Stamina = 100f;
+    public GameObject player;
+
+    [Header("Stamina Bar")]
+    public Image staminaBar;
+    public float staminaAmount = 200f;
+    public float staminaRegenSpeed;
+    public float sprintCooldown;
+    bool sprintReady;
+
+    private Movement Movement;
+
+    private void Awake()
+    {
+        Movement = player.GetComponent<Movement>();
+        sprintReady = true;
+    }
+
     void Update()
     {
-        
+
+        if (Movement.moveSpeedMultiplier > 1)
+        {
+            StaminaDeplete(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            StaminaRegen(2);
+        }
+
+        if (staminaAmount <= 0)
+        {
+            sprintReady = false;
+
+            staminaAmount = 0;
+
+            Movement.moveSpeedMultiplier = 1;
+            Invoke(nameof(SprintReset), sprintCooldown);
+        }
+
+        if (staminaAmount < 200 && Movement.moveSpeedMultiplier == 1 && sprintReady == true)
+        {
+            StaminaRegen(1);
+        }
     }
 
-    private void StaminaDeplete()
+    private void StaminaDeplete(float deplete)
     {
-
+        staminaAmount -= deplete;
+        staminaBar.fillAmount = staminaAmount / 200f;
     }
 
-    private void StaminaRegen()
+    private void StaminaRegen(float regen)
     {
+        staminaAmount += regen;
+        staminaAmount = Mathf.Clamp(staminaAmount, 0, 200);
 
+        staminaBar.fillAmount = staminaAmount / 200f;
+    }
+
+    private void SprintReset()
+    {
+        sprintReady = true;
+        staminaAmount = 2;
     }
 }
