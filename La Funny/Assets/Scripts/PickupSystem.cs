345add631808dbaa33dcf780681740c089187cb9
Bushman-31 <513323@vistacollege.nl>
2023-10-23 13:03:50 +0200

    update pickup

--- La Funny/Assets/Scripts/PickupSystem.cs
+++ La Funny/Assets/Scripts/PickupSystem.cs
@@ -12,25 +12,26 @@ public class PickupClass : MonoBehaviour
     [SerializeField] private float PickupRange;
     private Rigidbody CurrentObject;
     private void Start()
-    { PlayerCamera=GetComponent<Camera>(); }
-    
+    { PlayerCamera = GetComponent<Camera>(); }
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E)) 
+        if (Input.GetKeyDown(KeyCode.E))
         {
 
-            if(CurrentObject)
+            if (CurrentObject)
             {
                 CurrentObject.useGravity = true;
                 CurrentObject = null;
                 return;
             }
-            Debug.DrawRay(new Vector3(0.5f, 0.5f, 0f),PlayerCamera.transform.forward*1, Color.red,10);
+            Debug.Log("bugRay");
+            Debug.DrawRay(new Vector3(0.5f, 0.5f, 0f), PlayerCamera.transform.forward * 1, Color.red, 10);
             Ray CameraRay = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-            if(Physics.Raycast(CameraRay,out RaycastHit HitInfo, PickupRange, PickupMask))
+            if (Physics.Raycast(CameraRay, out RaycastHit HitInfo, PickupRange, PickupMask))
             {
                 CurrentObject = HitInfo.rigidbody;
-                CurrentObject.useGravity = false;
+                CurrentObject.useGravity = true;
             }
         }
     }
@@ -39,10 +40,12 @@ public class PickupClass : MonoBehaviour
     {
         if (CurrentObject)
         {
-            Vector3 DirectionToPoint = PickupTarget.position - CurrentObject.position;
+            Debug.Log("Bugg");
+            Vector3 DirectionToPoint = PickupTarget.position - CurrentObject.transform.forward;
             float DistanceToPoint = DirectionToPoint.magnitude;
 
-            CurrentObject.velocity = DirectionToPoint * 12f * DistanceToPoint;
+            DirectionToPoint = PickupTarget.position;
+
         }
     }
 }
