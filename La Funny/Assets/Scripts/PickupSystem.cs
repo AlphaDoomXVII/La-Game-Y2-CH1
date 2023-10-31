61bd3cb31ea7c4f6c44ac647a2e4f4fdb15b8d21
Bushman-31 <513323@vistacollege.nl>
2023-10-31 10:09:45 +0100

    empty

--- La Funny/Assets/Scripts/PickupSystem.cs
+++ La Funny/Assets/Scripts/PickupSystem.cs
@@ -4,41 +4,24 @@ using UnityEngine;
 
 public class PickupClass : MonoBehaviour
 {
+    public Camera player;
+    public GameObject pickupfloat;
+    
 
-    [SerializeField] private LayerMask PickupMask;
-    [SerializeField] private Camera PlayerCamera;
-    [SerializeField] private Transform PickupTarget;
-    [Space]
-    [SerializeField] private float PickupRange;
-    private Rigidbody CurrentObject;
 
     private void Start()
-    { PlayerCamera = GetComponent<Camera>(); }
+    {
+
+    }
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-
-            if (CurrentObject)
-            {
-                CurrentObject.useGravity = true;
-                CurrentObject = null;
-                return;
-            }
-            Debug.Log("bugRay");
-            Debug.DrawRay(new Vector3(0.5f, 0.5f, 0f), PlayerCamera.transform.forward * 1, Color.red, 10);
-            Ray CameraRay = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-            if (Physics.Raycast(CameraRay, out RaycastHit HitInfo, PickupRange, PickupMask))
-            {
-                CurrentObject = HitInfo.rigidbody;
-                CurrentObject.useGravity = false;
-            }
-        }
+        
     }
 
-   void FixedUpdate()
+    void FixedUpdate()
     {
-       
+        
     }
 }
