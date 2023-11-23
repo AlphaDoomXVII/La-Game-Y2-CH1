ae8259ea1163fefe928389765bd65ca4d94cf3f0
Doggo-Uchiha <113337477+LucasPeters268@users.noreply.github.com>
2023-11-23 10:10:27 +0100

    Comments about enemy spawner
    Documentation about enemy spawner script has been added

--- La Funny/Assets/Scripts/Enemy's/SpawnManager.cs
+++ La Funny/Assets/Scripts/Enemy's/SpawnManager.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager : MonoBehaviour   //laat je code toevoegen aan het game object " SpawnManager "//
 {
-    public GameObject enemyPrefab;
-    public Transform spawnPoint;
+    public GameObject enemyPrefab;      //Welke object word er gespawnt wanneer het script getriggerd word//
+    public Transform spawnPoint;        //laat het object weten dat het op het "spawnpoint" moet spawnen
 
-    void Update()
+    void Update()   //kijkt of er code door komt (maar er komt niks door)
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F)) //De action key om het script te triggeren
         {
-            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint.parent);
+            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint.parent); //wanneer de F key gebruikt word, word  de enemy object op zijn parent (spawnpoint) gespawnt.
         }
     }
 }
---
+++ La Funny/Assets/Scripts/EnemySpawnSystem.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnSystem : MonoBehaviour
+[SerializeField] private Transform[] spawners;
