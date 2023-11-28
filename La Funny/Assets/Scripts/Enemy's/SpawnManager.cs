931268ffdcd1c4a96d3b992777b37db89788f355
Doggo-Uchiha <113337477+LucasPeters268@users.noreply.github.com>
2023-11-28 12:31:59 +0100

    Enemy spawner error fixed

--- La Funny/Assets/Scripts/Enemy's/SpawnManager.cs
+++ La Funny/Assets/Scripts/Enemy's/SpawnManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour   //laat je code toevoegen aan het game object " SpawnManager "//
+public class SpawnManager : MonoBehaviour   //laat je code toevoegen aan het game object " SpawnManager "
 {
-    public GameObject enemyPrefab;      //Welke object word er gespawnt wanneer het script getriggerd word//
+    public GameObject enemyPrefab;      //Welke object word er gespawnt wanneer het script getriggerd word
     public Transform spawnPoint;        //laat het object weten dat het op het "spawnpoint" moet spawnen
 
     void Update()   //kijkt of er code door komt (maar er komt niks door)
--- La Funny/Assets/Scripts/EnemySpawnSystem.cs
+++ La Funny/Assets/Scripts/EnemySpawnSystem.cs
@@ -3,4 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnSystem : MonoBehaviour
+{ 
 [SerializeField] private Transform[] spawners;
+}
